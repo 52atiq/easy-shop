5695eeb3dadcf3a4817df2154cdcdf136628ed06
unknown <atiqit52@gmail.com>
2022-07-19 15:49:04 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Carousel from './components/Carousel/Carousel';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1 className='text-red-600 text-3xl'> Carousel </h1>
+     <Carousel></Carousel>
+   
+   
+     
     </div>
   );
 }
